3f995223637c09c28977210cabbbd00e18904c03
Asish <ashishshah0900@gmail.com>
2023-10-14 17:24:54 +0530

    Project Repo Creation

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-center">
+      <a
+        href="https://www.linkedin.com/in/ashish-kumar-shah/"
+        className="fa fa-linkedin"
+      >
+        LinkedIn
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
